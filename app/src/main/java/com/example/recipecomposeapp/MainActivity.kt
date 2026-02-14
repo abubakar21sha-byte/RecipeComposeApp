e30d52644f573616238788f04106ac3ac96eac01
Web-Entwickler <pixpox21@bk.ru>
2026-02-14 20:33:59 +0100

    RCA-04: organize app structure with Scaffold

--- app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
@@ -14,20 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
-//Задача
-//Добавить базовую структуру приложения через Scaffold.
-//
-//В MainActivity.onCreate после enableEdgeToEdge() добавить setContent:
-//
-//Обернуть в стандартную MaterialTheme { }
-//Внутри добавить Scaffold { paddingValues -> }
-//В content показать Text('Recipes App', modifier = Modifier.padding(paddingValues))
-//Проверить работу:
-//
-//Запустить приложение
-//Текст не должен перекрываться системной панелью
-//Scaffold автоматически применяет отступы
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
